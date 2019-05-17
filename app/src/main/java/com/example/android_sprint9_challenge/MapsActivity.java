f2be7cce779c1a1e87d56bdfb6f54c6bcb879837
KyleCole01 <kyle@misfitselectronics.com>
2019-05-17 11:45:26 -0400

    generated map layout file and added media player functionality with the pin droop raw file

--- app/src/main/java/com/example/android_sprint9_challenge/MapsActivity.java
+++ app/src/main/java/com/example/android_sprint9_challenge/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.example.android_sprint9_challenge;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,37 +16,49 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    MediaPlayer mediaPlayer;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        context = this;
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.center_map:
+                break;
+            case R.id.place_pin:
+                LatLng position = mMap.getCameraPosition().target;
+                mMap.addMarker(new MarkerOptions().position(position).title("Custom Marker"));
+                mediaPlayer = MediaPlayer.create(context, R.raw.pin_drop);
+                mediaPlayer.start();
+        }
+        return true;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng random = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(random).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(random));
     }
 }
