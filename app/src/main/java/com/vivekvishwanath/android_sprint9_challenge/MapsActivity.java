a730cabed273097702795b7aca1cdbfbc4f0c14c
VivekV95 <vivekv210@gmail.com>
2019-05-17 11:03:02 -0500

    Can now toggle camera and zoom on current location

--- app/src/main/java/com/vivekvishwanath/android_sprint9_challenge/MapsActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_sprint9_challenge/MapsActivity.java
@@ -1,22 +1,39 @@
 package com.vivekvishwanath.android_sprint9_challenge;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Context context;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LatLng myLocation;
+    private static final int FINE_LOCATION_REQUEST_CODE = 1;
+    private static final int MAP_CAMERA_ZOOM = 10;
+    private static final int MAP_CAMERA_ZOOM_OUT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +45,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        context = this;
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
     }
 
 
@@ -43,10 +63,63 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_center) {
+            if (!item.isChecked()) {
+                //check for permission
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // need to request permission
+                    ActivityCompat.requestPermissions((Activity) context
+                            , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+                } else {
+                    //permission already granted
+                    getLocation();
+                    item.setChecked(true);
+                }
+            } else {
+                item.setChecked(false);
+                LatLng defaultLocation = new LatLng(0,0);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, MAP_CAMERA_ZOOM_OUT));
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, MAP_CAMERA_ZOOM));
+                }
+            }
+        });
     }
 }
