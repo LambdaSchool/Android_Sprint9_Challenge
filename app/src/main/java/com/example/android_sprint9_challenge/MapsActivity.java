dc5f1c4c402e43dc2ad19617fa0a70f65fc24468
KyleCole01 <kyle@misfitselectronics.com>
2019-05-17 12:08:31 -0400

    MVP finished, has toolbar actions (with icons) that center map and drop pin. Dropping pin makes the pindrop sound. Stretches - Long press for pin drop outside of center of screen (with its own sound). Added a numbering system for the pin drops and a naming scheme for clicked vs action bar markers.

--- app/src/main/java/com/example/android_sprint9_challenge/MapsActivity.java
+++ app/src/main/java/com/example/android_sprint9_challenge/MapsActivity.java
@@ -1,32 +1,47 @@
 package com.example.android_sprint9_challenge;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    public static final int LOCATION_REQUEST_CODE = 1;
+    GoogleMap mMap;
     MediaPlayer mediaPlayer;
     Context context;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    int markerNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         context = this;
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -43,12 +58,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.center_map:
+                if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions((Activity)context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                }else{
+                    getLocation();
+                }
                 break;
             case R.id.place_pin:
                 LatLng position = mMap.getCameraPosition().target;
-                mMap.addMarker(new MarkerOptions().position(position).title("Custom Marker"));
+                markerNumber++;
+                mMap.addMarker(new MarkerOptions().position(position).title("Added marker "+ markerNumber));
                 mediaPlayer = MediaPlayer.create(context, R.raw.pin_drop);
                 mediaPlayer.start();
+
         }
         return true;
     }
@@ -57,8 +79,38 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng random = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(random).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(random));
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng).title("clicked marker").draggable(true));
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == LOCATION_REQUEST_CODE){
+            if(permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLocation();
+            }
+        }
+    }
+
+
+    public void getLocation(){
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 10f));
+                mMap.addMarker(new MarkerOptions().position(position).title("Your Location"));
+            }
+        });
     }
 }
