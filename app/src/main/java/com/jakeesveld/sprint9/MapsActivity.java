284c604986860c77e2886ea4c4c8d69c4c560f8c
Jake Esveld <thejakeesveld@gmail.com>
2019-05-17 08:27:25 -0700

    Add functionality to drop pin on current camera position with sound effect

--- app/src/main/java/com/jakeesveld/sprint9/MapsActivity.java
+++ app/src/main/java/com/jakeesveld/sprint9/MapsActivity.java
@@ -1,10 +1,15 @@
 package com.jakeesveld.sprint9;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,14 +18,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    MediaPlayer mediaPlayer;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        context = this;
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -31,6 +41,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.maps_options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.options_center_map:
+                break;
+            case R.id.options_drop_pin:
+                LatLng position = mMap.getCameraPosition().target;
+                mMap.addMarker(new MarkerOptions().position(position).title("Custom Marker"));
+                mediaPlayer = MediaPlayer.create(context, R.raw.pin_drop);
+                mediaPlayer.start();
+        }
         return true;
     }
 
