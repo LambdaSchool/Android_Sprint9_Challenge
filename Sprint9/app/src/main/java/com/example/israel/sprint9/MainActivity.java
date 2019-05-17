70481bb2f23d68214c3e203c5d330361927738f7
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-17 11:31:38 -0500

    audio settings added

--- Sprint9/app/src/main/java/com/example/israel/sprint9/MainActivity.java
+++ Sprint9/app/src/main/java/com/example/israel/sprint9/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.israel.sprint9;
 
 import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -23,11 +27,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, SettingsFragment.OnSelectAudioInterface {
 
+    private static final int REQUEST_MARKER_DROP_AUDIO = 1;
     private GoogleMap googleMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private MediaPlayer mediaPlayer;
+    private SettingsFragment.OnAudioSelectedListener audioSelectedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_MARKER_DROP_AUDIO) {
+            audioSelectedListener.onAudioSelected(data.getData());
+        }
     }
 
     @Override
@@ -62,6 +77,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             case R.id.main_toolbar_add_marker: {
                 addMarker();
+            } break;
+
+            case R.id.main_toolbar_settings: {
+                SettingsFragment settingsFragment = SettingsFragment.newInstance();
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.activity_main_constraint_root, settingsFragment)
+                        .addToBackStack(null)
+                        .commit();
             } break;
         }
 
@@ -107,8 +130,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             mediaPlayer = null;
         }
 
-        mediaPlayer = MediaPlayer.create(this, Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.pin_drop));
+        Uri audioUri = MarkerDropAudioSPDAO.getMarkerDropAudio(this);
+        //Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.pin_drop)
+        if (audioUri == null) {
+            return;
+        }
+
+        mediaPlayer = MediaPlayer.create(this, audioUri);
         mediaPlayer.start();
 
+    }
+
+    @Override
+    public void onSelectAudio(SettingsFragment.OnAudioSelectedListener l) {
+        audioSelectedListener = l;
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+
+        ComponentName componentName = intent.resolveActivity(getPackageManager());
+        if (componentName != null) {
+            startActivityForResult(intent, REQUEST_MARKER_DROP_AUDIO);
+        } else {
+            Toast toast = Toast.makeText(this, "No activity", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
