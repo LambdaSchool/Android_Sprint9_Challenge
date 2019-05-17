b036c3356c9eadf9cca1365ced3db18f703fbb12
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-17 10:32:25 -0500

    added map and toolbar

--- Sprint9/app/src/main/java/com/example/israel/sprint9/MainActivity.java
+++ Sprint9/app/src/main/java/com/example/israel/sprint9/MainActivity.java
@@ -2,13 +2,72 @@ package com.example.israel.sprint9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.activity_main_map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_toolbar_menu, menu);
+
+        return true;
+    }
+
+    private void onMenuItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        switch (id) {
+            case R.id.main_toolbar_center: {
+                center();
+            } break;
+
+            case R.id.main_toolbar_add_marker: {
+                addMarker();
+            } break;
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+    }
+
+    private void center() {
+        if (googleMap == null) {
+            return;
+        }
+
+
+    }
+
+    private void addMarker() {
+        if (googleMap == null) {
+            return;
+        }
+
 
     }
 }
