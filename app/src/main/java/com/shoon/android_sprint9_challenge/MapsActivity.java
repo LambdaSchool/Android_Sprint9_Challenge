e554b218f8c1de5e701f4633ecbac59c533f9080
Shoonsk8 <shoon@att.net>
2019-05-24 17:17:29 -0400

    my location added

--- app/src/main/java/com/shoon/android_sprint9_challenge/MapsActivity.java
+++ app/src/main/java/com/shoon/android_sprint9_challenge/MapsActivity.java
@@ -1,6 +1,9 @@
 package com.shoon.android_sprint9_challenge;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -9,6 +12,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +21,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,13 +37,21 @@ public class MapsActivity extends AppCompatActivity implements
         GoogleMap.OnMapClickListener,
         GoogleMap.OnMapLongClickListener,
         GoogleMap.OnCameraIdleListener,
-        OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
+        OnMapReadyCallback,
+        GoogleMap.OnMyLocationClickListener,
+        GoogleMap.OnMyLocationButtonClickListener,
+
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        NavigationView.OnNavigationItemSelectedListener {
 
     private GoogleMap mMap;
     private static final int MY_LOCATION_PERMISSION_REQUEST_CODE = 1;
     private static final int LOCATION_LAYER_PERMISSION_REQUEST_CODE = 2;
     private boolean mLocationPermissionDenied = false;
     MediaPlayer player;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
+    private LatLng currentLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
@@ -91,7 +105,7 @@ public class MapsActivity extends AppCompatActivity implements
         return true;
     }
 
-    boolean bPin=false;
+    boolean bPin=true;
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -100,6 +114,15 @@ public class MapsActivity extends AppCompatActivity implements
                 break;
             case R.id.disablePin:
                 bPin=false;
+                break;
+            case R.id.addPin:
+                if(currentLocation!=null){
+                    mMap.addMarker( new MarkerOptions()
+                            .position(currentLocation).title( "my location" )
+                            .snippet(currentLocation.toString()) );
+                    player.start();
+                }
+
                 break;
         }
         return true;
@@ -127,7 +150,9 @@ public class MapsActivity extends AppCompatActivity implements
         mMap.addMarker( new MarkerOptions().position( sydney ).title( "Marker in Sydney" ) );
         mMap.moveCamera( CameraUpdateFactory.newLatLng( sydney ) );
         mMap.getUiSettings().setZoomControlsEnabled(true);
-
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        enableMyLocation();
         UiSettings mUiSettings = mMap.getUiSettings();
         mUiSettings.setMapToolbarEnabled(true);
         mUiSettings.setZoomControlsEnabled(true);
@@ -155,12 +180,22 @@ public class MapsActivity extends AppCompatActivity implements
                     .position(latLng).title( Integer.toString( iCounter++ ) )
                     .snippet( latLng.toString()) );
             player.start();
-
+            currentLocation=latLng;
         }
 
     }
 
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        currentLocation=new LatLng( location.getLongitude(),location.getLongitude());
 
+        mMap.addMarker( new MarkerOptions()
+                .position(currentLocation).title(  "My location" )
+                .snippet(currentLocation.toString()) );
+        player.start();
+
+    //    Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         return false;
@@ -169,5 +204,56 @@ public class MapsActivity extends AppCompatActivity implements
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Location location = mMap.getMyLocation();
+        currentLocation=new LatLng( location.getLatitude(),location.getLongitude());
+        return false;
     }
 }
