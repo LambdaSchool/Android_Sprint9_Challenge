c308f51de8f8f4dc47aecf89d8dce478a2cd67e7
Shoonsk8 <shoon@att.net>
2019-05-24 12:16:56 -0400

    Adding markers

--- app/src/main/java/com/shoon/android_sprint9_challenge/MapsActivity.java
+++ app/src/main/java/com/shoon/android_sprint9_challenge/MapsActivity.java
@@ -2,6 +2,7 @@ package com.shoon.android_sprint9_challenge;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +11,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnCameraIdleListener,
+        OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -37,10 +39,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnCameraIdleListener(this);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng( -34, 151 );
         mMap.addMarker( new MarkerOptions().position( sydney ).title( "Marker in Sydney" ) );
         mMap.moveCamera( CameraUpdateFactory.newLatLng( sydney ) );
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+       // addMarkersToMap();
     }
+
+    @Override
+    public void onCameraIdle() {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    int iCounter=0;
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mMap.addMarker( new MarkerOptions()
+                .position(latLng).title( Integer.toString( iCounter++ ) )
+                .snippet( latLng.toString()).draggable(true ) );
+
+    }
+
 }
