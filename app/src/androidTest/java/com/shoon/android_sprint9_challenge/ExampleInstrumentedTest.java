45dac00cde2943d31ed8d8efd1ac9a3693113ce8
Shoonsk8 <shoon@att.net>
2019-05-24 11:43:49 -0400

    blank

--- app/src/androidTest/java/com/shoon/android_sprint9_challenge/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/shoon/android_sprint9_challenge/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals( "com.shoon.android_sprint9_challenge", appContext.getPackageName() );
+        assertEquals("com.shoon.android_sprint9_challenge", appContext.getPackageName());
     }
 }
