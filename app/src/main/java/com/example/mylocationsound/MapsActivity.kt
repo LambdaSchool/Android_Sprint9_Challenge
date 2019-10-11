3119c337c658f76a5cec6de4ac29d2ea8cad246c
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-11 11:00:08 -0600

    My Location Sound
    Bind the toolbar to activity

--- app/src/main/java/com/example/mylocationsound/MapsActivity.kt
+++ app/src/main/java/com/example/mylocationsound/MapsActivity.kt
@@ -2,6 +2,11 @@ package com.example.mylocationsound
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+
+import androidx.drawerlayout.widget.DrawerLayout
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,21 +14,48 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.navigation.NavigationView
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private var drawerLayout: DrawerLayout? = null
+
+    private var currentType: Int = 0
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        // this will assign our toolbar xml to be the system toolbar
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+      //  setSupportActionBar(toolbar)
+        toolbar.title = title
+        // TODO 3: get handle to drawer layout and bind to toolbar toggle
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            when (menuItem.itemId){
+                R.id.mSeeMyLocation -> {
+                    //do this
+                }
+            }
+            true
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
+
 
     /**
      * Manipulates the map once available.
